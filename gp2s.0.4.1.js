e3c35117b14c88e0270c470e335dea9bf550b003
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-18 21:58:14 +0300

    Update gp2s.0.4.1.js

--- gp2s.0.4.1.js
+++ gp2s.0.4.1.js
@@ -1,2 +1,2 @@
   document.getElementById('group_interaction_info_form-tab_view-estimated_end_date_out').click();
- document.getElementById("group_interaction_info_form-tab_view-estimated_end_date_out").value = text, + hour           +            ":" + minutes + " " + day + "." + month + "." + year    }, 1000);
+ 
