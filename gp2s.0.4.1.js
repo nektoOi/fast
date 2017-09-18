154d2c6e571db99a3ad55586162589e2b5b3a17a
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-18 22:27:50 +0300

    Update gp2s.0.4.1.js

--- gp2s.0.4.1.js
+++ gp2s.0.4.1.js
@@ -3,6 +3,4 @@ document.getElementById('group_interaction_info_form-tab_view-estimated_end_date
 }, 2000);
 setTimeout(function() { 
         document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value =  " Ожидаемое время восстановления ";
-           
-
 }, 3000);
