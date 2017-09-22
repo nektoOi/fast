a947889c1af3aac4d264ee82c56b5d935aa97e79
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-22 22:18:12 +0300

    Update gp2s.0.4.1.js

--- gp2s.0.4.1.js
+++ gp2s.0.4.1.js
@@ -7,9 +7,9 @@
     day < 10 && (day = "0" + day), month < 10 && (month = "0" + month), minutes < 10 && (minutes = "0" + minutes), hour < 10 && (hour = "0" + hour);
 
 
-//setTimeout(function() { 
-//document.getElementById('group_interaction_info_form-tab_view-estimated_end_date_out').click();
-//}, 1000);
+setTimeout(function() { 
+document.getElementById('group_interaction_info_form-tab_view-estimated_end_date_out').click();
+}, 1000);
 //setTimeout(function() { 
 //document.getElementById("group_interaction_info_form-tab_view-estimated_end_date_input").value =  day + "." + month + "." + year + " " +  hour + ":" + minutes;
 //}, 1000);
@@ -17,9 +17,9 @@
 //$( "button[class='ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-inplace-save']" ).click();
 //}, 2000);
 
-setTimeout(function() { 
-document.getElementById('group_interaction_info_form-tab_view-commentary_out').click();
-}, 1000);
+//setTimeout(function() { 
+//document.getElementById('group_interaction_info_form-tab_view-commentary_out').click();
+//}, 1000);
 
 //var text = document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value;
 
@@ -30,7 +30,3 @@ document.getElementById('group_interaction_info_form-tab_view-commentary_out').c
 //setTimeout(function() { 
 //var elm $("ui-inplace-display").addClass("ui-inplace-display ui state-highlight").click();
 //}, 2000);
-/
-//setTimeout(function() { 
-//document.getElementById('group_interaction_info_form-tab_view-j_idt229_display').click();
-//}, 1000);
