bc78b7130c6b29be7b586a1a7cda5d4fd4fe1074
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-25 18:29:00 +0300

    Update gp2s.0.4.js

--- gp2s.0.4.js
+++ gp2s.0.4.js
@@ -31,7 +31,7 @@ if (!$( "div[class='ui-grid-col-9']" )[6]) {
             reg = 'Ульяновский филиал';
             $( "tr[data-label='ОМССиИС МРФ Волга']" )[0].click();
         }
-	     if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value.indexOf('Мордовия  РЕСП') > 1) {
+	     if (document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value.indexOf('Мордовия') > 1) {
             reg = 'Республика Мордовия';
             $( "tr[data-label='ОМССиИС МРФ Волга']" )[0].click();
         }
