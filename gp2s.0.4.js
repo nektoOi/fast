f013d2a641817eeea65fb6237aa94c6216dc6230
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-26 20:39:59 +0300

    Update gp2s.0.4.js

--- gp2s.0.4.js
+++ gp2s.0.4.js
@@ -47,7 +47,7 @@ if (!$( "div[class='ui-grid-col-9']" )[6]) {
         }
         var text = reg + ", отсутствуют услуги у абонентов " + document.getElementById("group_interaction_info_form-tab_view-node_rule_building-node_rule_building_input").value;
         document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value = text, document.getElementById("group_interaction_info_form-tab_view-commentary").value = text + " Ожидаемое время восстановления " + hour           +            ":" + minutes + " " + day + "." + month + "." + year    }, 1000);
-    alert($(".ui-grid-col-7")[0].innerText.replace(' - Регистрация', ''));
+    alert($( "span[class='heading-accent']" )[0].innerText.replace(' - Регистрация', ''));
     setTimeout(function() {
         $( "button[class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only ui-priority-primary']" ).click();
     }, 5000);
