8c04a4fa5d33c5388aa303cdfcca858e94087b64
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-26 20:21:33 +0300

    Update gp2s.0.4.js

--- gp2s.0.4.js
+++ gp2s.0.4.js
@@ -49,6 +49,6 @@ if (!$( "div[class='ui-grid-col-9']" )[6]) {
         document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value = text, document.getElementById("group_interaction_info_form-tab_view-commentary").value = text + " Ожидаемое время восстановления " + hour           +            ":" + minutes + " " + day + "." + month + "." + year    }, 1000);
     alert($(".ui-grid-col-7")[0].innerText.replace(' - Регистрация', ''));
     setTimeout(function() {
-        document.getElementById('global_editor-form-save').click();
+        $( "button[class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only ui-priority-primary']" ).click();
     }, 5000);
 }
