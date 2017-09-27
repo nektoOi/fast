9e7a9b348ecedac356ac7eb19c32810c5dac3d7e
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-09-27 03:41:58 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,18 +2,18 @@ mainUrl='https://raw.githubusercontent.com/nektoOi/fast/master/';
 
 function httpGet(theUrl)
 {
-	//var xmlHttp = new XMLHttpRequest();
-//xmlHttp.open("GET", "/query", theUrl, false); // async=true
-//xmlHttp.onload = function (e) {
-  //  if xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-    //    console.log(xmlHttp.responseText);
-    //}
-//};
-//xmlHttp.send(null);
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); 
-	xmlHttp.send( null );
-   return xmlHttp.responseText;
+	var xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", "/query", theUrl, false); // async=true
+xmlHttp.onload = function (e) {
+  if xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        console.log(xmlHttp.responseText);
+    }
+};
+xmlHttp.send(null);
+   // var xmlHttp = new XMLHttpRequest();
+   // xmlHttp.open( "GET", theUrl, false ); 
+	//xmlHttp.send( null );
+  // return xmlHttp.responseText;
 }
 
 
